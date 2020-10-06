ec711485c15dfd74f5e81b27df01a704d4f508ab
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2020-10-06 19:18:32 +0200

    initial commit

---
+++ instaClone/instaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaClone
+//
+//  Created by Marina Beatriz Santana de Aguiar on 06.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
